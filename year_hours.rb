dcb5eb2d7230a44a842436ff2bcf26b17e01e142
miller <millercarreno@seven4n.com>
2018-09-11 06:12:57 -0500

    how many hours has a year?

---
+++ year_hours.rb
@@ -0,0 +1,11 @@
+def hours_of_year
+ day_hours = 24
+ # hour_mins = 60
+ year_days = 365
+ year_hours = day_hours * year_days
+ puts ("---")
+ puts ("The hours of the regular year are: " + year_hours.to_s)
+ puts ("---")
+end
+
+hours_of_year
