3aaf236f01dcca6ac88c3d22afcc13c4e838438c
miller <millerpoli9@gmail.com>
2018-11-12 00:08:05 -0500

    simple ruby app to scan ports

--- scanning_ports.rb
+++ scanning_ports.rb
@@ -1,13 +1,13 @@
 require 'socket'
 require 'timeout'
 
-print "Please enter the host you're going to scan: "
+puts "\nPlease enter the host/ip you're going to scan: "
 ip = gets.chomp
 
-print = "Enter the Initial Port: "
+print = "\nEnter the Initial Port: "
 start_port = gets.chomp
 
-print "Enter the end port:"
+print "\nEnter the end port: "
 end_port = gets.chomp
 
 # from start_port to end_port:
@@ -16,17 +16,13 @@ ports = start_port..end_port
 ports.each do |port|
  begin
   # Timeout with 15 as time limit -> new request from TCPSocket
-  Timeout::timeout(15){TCPSocket.new("#{ip}"), port)}
+  Timeout::timeout(15){TCPSocket.new("#{ip}", port)}
  rescue
   puts "The port number #{port} is closed"
  else
   puts "The port number #{port} is open"
  end
-
-
-
-
-
+end
 
 
 
