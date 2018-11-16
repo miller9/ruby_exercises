6e802119e1c3d57f7364cbbd6e917f09118b2c42
miller <millerpoli9@gmail.com>
2018-11-15 21:17:54 -0500

    adding print methods

--- nested_array.rb
+++ nested_array.rb
@@ -33,8 +33,12 @@ puts ""
 puts "\nAdding a new element to the matrix with 'push':"
 nested_array.push("whale")
 
-puts "\n The new nested_array:"
+puts "\n The new nested_array: (with 'puts')"
 puts nested_array
+puts
+
+puts "\n The new nested_array: (with 'print')"
+print nested_array
 puts
 
 
@@ -45,4 +49,7 @@ puts "\n The new nested_array:"
 puts nested_array
 puts
 
+puts "\n The new nested_array: (with 'print')"
+print nested_array
+puts
 
