43866d897dd7bae3be955fc0fb2e4826da4eb8b5
miller <millerpoli9@gmail.com>
2018-11-15 21:11:00 -0500

    Adding a simple nested array eg

---
+++ nested_array.rb
@@ -0,0 +1,36 @@
+nested_array = [
+    [
+        "salmon",
+        "halibut",
+    ],
+    [
+        "coral",
+        "reef",
+    ]
+]
+
+puts "\nPrinting the nested_array: (with 'puts')"
+puts nested_array
+
+puts "\nPrinting the nested_array: (with 'print')"
+print nested_array
+puts ""
+
+puts"\nPrinting the 'coral' value: (with 'puts')"
+puts nested_array[1][0];
+
+puts"\nPrinting the 'coral' value: (with 'print')"
+print nested_array[1][0];
+puts ""
+
+puts "\nPrinting the first element: (with 'puts')"
+puts nested_array[0]
+
+puts "\nPrinting the first element: (with 'print')"
+print nested_array[0]
+puts ""
+
+
+
+
+
