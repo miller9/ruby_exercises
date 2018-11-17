b2988c7e034827a24f39c0deed5ba967a5651d6e
miller <millerpoli9@gmail.com>
2018-11-16 19:44:51 -0500

    some basic methods

--- arrays2.rb
+++ arrays2.rb
@@ -6,5 +6,9 @@ puts
 puts "\nfirst shark: " + sharks[0]
 puts "second shark: " + sharks[1]
 puts "third shark: " + sharks[2]
+puts "size of the array: " + sharks.length.to_s
+print 'sharks.index("Tiger") --> ' + sharks.index("Tiger").to_s
+puts 
+
 
 
