d3ccc77cd32e020a4e6234ef85811479e120a8df
miller <millerpoli9@gmail.com>
2018-11-16 19:40:59 -0500

    simple array eg

---
+++ arrays2.rb
@@ -0,0 +1,10 @@
+puts "\nSome shark's array"
+sharks = ["Hammerhead", "Great White", "Tiger"]
+print sharks
+puts
+
+puts "\nfirst shark: " + sharks[0]
+puts "second shark: " + sharks[1]
+puts "third shark: " + sharks[2]
+
+
