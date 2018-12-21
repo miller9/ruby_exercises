eb4655153c248ea7b1408ca3d9eddad94bc12b25
miller <millerpoli9@gmail.com>
2018-12-21 18:18:01 -0500

    lowering, uppering, capitalizing strings

---
+++ lower_upper_capitalizes.rb
@@ -0,0 +1,9 @@
+puts """
+Lower case, upper case and capitalizes all the words of a given string
+"""
+puts ""
+print("ruby exercises!".upcase)
+print("\n","Ruby Exercises!".downcase)
+print("\n","ruby exercises!".capitalize)
+
+puts ""
