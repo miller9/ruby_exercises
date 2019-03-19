61e081f44b7631b506382cefd8474f2f61e213b9
miller9 <millerpoli9@gmail.com>
2019-03-18 23:22:37 -0500

    Same exercise with TIME --> Adding simple exercise with multiples of 3, 5 and both

---
+++ FizzBuzzExercise_with_TIMES.rb
@@ -0,0 +1,20 @@
+puts
+puts """ This is a problem used by lots of employers as a warm up exercise during an interview. Write some code in Ruby that prints numbers from 1 to 100, BUT for multiples of three, print 'fizz', for multiples of 5 print 'buzz', and for multiples of both print 'fizzbuzz'.
+"""
+puts
+def fizzBuzzExercise
+    i = 1
+    100.times do |number|
+        if i % 3 == 0
+            if i % 5 == 0
+                puts i.to_s + " fizzbuzz --> multiples of both"
+            else
+                puts i.to_s + " fizz --> multiples of three"
+            end
+        elsif i % 5 == 0
+            puts i.to_s + " buzz --> multiples of five"
+        end
+        i += 1
+    end
+end
+fizzBuzzExercise
