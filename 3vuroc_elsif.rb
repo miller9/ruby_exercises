17f5eb20fe96b34d1959e46b3835a7ee88cc3b1a
miller9 <millerpoli9@gmail.com>
2019-03-20 19:11:18 -0500

    courseScript3

---
+++ 3vuroc_elsif.rb
@@ -0,0 +1,42 @@
+# ELSE IF:
+
+
+#si condicion
+#	si se cumple la condicion
+#si no entonces si condicion
+#	si se cumple la condicion
+#si no entonces si condicion
+#	si se cumple la condicion
+#si no entonces si condicion
+#	si se cumple la condicion
+#si no entonces si condicion
+#	si se cumple la condicion
+#si no, entonces si condicion
+#	si se cumple la condicion
+#entonces
+#   has esto
+#fin
+puts "\nIngresa un valor:"
+valor = gets.chomp.to_i
+if valor > 100
+  puts "el valor ingresado es mayor a 100"
+elsif  valor < 0
+  puts "el valor es menor a 0"
+else
+  puts "El valor esta en el rango de 0 a 100"
+end
+
+
+#si condicion
+#	si se cumple la condicion
+#entonces si condicion
+#	si se cumple la condicion
+#entonces si condicion
+#	si se cumple la condicion
+#entonces si condicion
+#	si se cumple la condicion
+#entonces si condicion
+#	si se cumple la condicion
+#entonces si condicion
+#	si se cumple la condicion
+#fin
