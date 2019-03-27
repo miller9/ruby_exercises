601283586e643a4f731248c0c8e67890ac308b61
miller9 <millerpoli9@gmail.com>
2019-03-26 19:22:23 -0500

    Times vuroc script

---
+++ 7vuroc_Times.rb
@@ -0,0 +1,15 @@
+# TIMES LOOP:
+#
+
+puts "\nFirst Loop:"
+var = 10
+var.times do |num|
+  puts num
+end
+
+puts "\nSecond Loop:"
+12.times do |number|
+  puts " something ".to_s + number.to_s
+end
+
+
