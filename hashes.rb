0aa06452d852c507175c4a3703b9bcf30945f5a2
miller9 <millerpoli9@gmail.com>
2019-04-17 21:04:20 -0500

    hashes

---
+++ hashes.rb
@@ -0,0 +1,22 @@
+puts "HASHES\n"
+dictionary = { "one" => "eins", "two" => "zwei", "three" => "drei" }
+dictionary["zero"] = "null"
+puts dictionary["zero"]
+
+puts
+
+dictionary = { "one" => "eins", "two" => "zwei", "three" => "drei" }
+dictionary["one"] = "uno"
+puts dictionary["one"]
+
+puts
+
+{ 1 => "eins", 2 => "zwei", 3 => "drei" }
+{ :one => "eins", :two => "zwei", :three => "drei" }
+{ "weights" => ["pound", "kilogram"], "lengths" => ["meter", "mile"] }
+{ :de => { :one => "eins", :two => "zwei", :three => "drei" } }
+
+
+puts
+
+
